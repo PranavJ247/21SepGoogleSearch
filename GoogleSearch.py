9c9be57f22f0f06d9162d8541e1a2437f3a3f5e8
Pranav Jadhav <142619252+PranavJ247@users.noreply.github.com>
2024-09-24 00:34:22 +0530

    Update GoogleSearch.py

--- GoogleSearch.py
+++ GoogleSearch.py
@@ -1,20 +1,10 @@
 import time
-from selenium import webdriver
-from selenium.webdriver import Keys
-from selenium.webdriver.chrome.service import Service
+khjhrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-# In this script will select client in configuration page
-
-service_obj = Service()
-driver = webdriver.Chrome(service=service_obj)
-driver.get("http://www.google.com")
-driver.maximize_window()
-
-time.sleep(5)
-dictVal = {1: 'kavita', 2: "password"}
+ugyg
 driver.find_element(By.XPATH, "//textarea[@id='APjFqb']").send_keys(dictVal[1])
 driver.find_element(By.XPATH, "//div[@class='FPdoLc lJ9FBc']//input[@name='btnK']").click()
 time.sleep(5)
